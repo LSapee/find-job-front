753e02a7a7e5d0eec379d286dd7a2eacb6999e34
LSapee <dlworhks94@gmail.com>
2024-04-30 22:04:14 +0900

    CI/CD 세팅중.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 빌드된 파일을 S3 버킷으로 업로드합니다.
-aws s3 sync build/ s3://${{ secrets.AWS_DISTRIBUTION_ID }}/ --acl public-read
+aws s3 sync build/ s3://${{ secrets.AWS_S3_BUCKET }}/ --acl public-read
 
 # CloudFront 캐시를 갱신합니다.
-aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_S3_BUCKET }} --paths "/*"
+aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_DISTRIBUTION_ID }} --paths "/*"
 
 echo "Deployment completed successfully."
 
