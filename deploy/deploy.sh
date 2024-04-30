51fb42e6724a370e706e01b5eaaf128bd7f2de72
LSapee <dlworhks94@gmail.com>
2024-04-30 23:51:46 +0900

    cicd test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 빌드된 파일을 S3 버킷으로 업로드합니다.
-aws s3 sync build/ s3://$AWS_S3_BUCKET/ --acl public-read
+aws s3 sync build/ s3://$AWS_S3_BUCKET/
 
 # CloudFront 캐시를 갱신합니다.
 aws cloudfront create-invalidation --distribution-id $AWS_DISTRIBUTION_ID --paths "/*"
