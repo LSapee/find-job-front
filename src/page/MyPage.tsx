9d554dc09160d63f088b471597986156912ac96d
LSapee <dlworhks94@gmail.com>
2024-05-07 20:23:36 +0900

    지원완료 회사 취소기능 추가

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -87,7 +87,40 @@ const MyPage:React.FC<UserProps> =({isLoggedIn}) => {
                 // 서버로부터 받은 데이터 처리
                 alert("처리 성공")
                 tt=true;
-                console.log('서버 응답 데이터:', data);
+            })
+            .catch(error => {
+                // 오류 처리
+                alert("처리 실패")
+                console.error('There was a problem with your fetch operation:', error);
+            });
+        if(tt){
+            const updatedJobs = ignoredJobs.filter(job => job.companyName !== companyName);
+            setIgnoredJobs(updatedJobs);
+        }
+
+    }
+    const companyCen = async (companyName:string) =>{
+        const delData = {
+            companyName: companyName
+        }
+        let tt:boolean=false
+        await fetch(`https://findjobapi.lsapee.com/api/companyT `,
+            // await fetch(`http://localhost:3001/api/companys `,
+            {method: 'DELETE',
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include',
+                body: JSON.stringify(delData),
+            }
+        ).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 형태로 응답 받기
+        })
+            .then(data => {
+                // 서버로부터 받은 데이터 처리
+                alert("처리 성공")
+                tt=true;
             })
             .catch(error => {
                 // 오류 처리
@@ -115,6 +148,7 @@ const MyPage:React.FC<UserProps> =({isLoggedIn}) => {
                                 <th scope="col">지원 공고명</th>
                                 <th scope="col">지원한 사이트</th>
                                 <th scope='col'>지원 날짜</th>
+                                <th scope='col'>삭제</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -125,6 +159,13 @@ const MyPage:React.FC<UserProps> =({isLoggedIn}) => {
                                     <td>{job.postTitle}</td>
                                     <td>{job.siteName}</td>
                                     <td>{job.date.substring(0, 10)}</td>
+                                    <td>
+                                        <button className="btn btn-danger" onClick={(e) => {
+                                            companyCen(job.companyName)
+                                        }}>
+                                            취소
+                                        </button>
+                                    </td>
                                 </tr>
                             ))}
                             </tbody>
